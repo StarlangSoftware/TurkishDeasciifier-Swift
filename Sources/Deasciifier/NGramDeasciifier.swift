8e499b4df1cc0b68c48d3b6c132dfc7ef2361504
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-09-28 21:47:05 +0300

    Removed extra repeat.

--- Sources/Deasciifier/NGramDeasciifier.swift
+++ Sources/Deasciifier/NGramDeasciifier.swift
@@ -76,70 +76,61 @@ public class NGramDeasciifier : SimpleDeasciifier{
      - Returns: Sentence result as output.
      */
     public override func deasciify(sentence: Sentence) -> Sentence {
-        var s : Sentence = sentence
-        var result : Sentence = Sentence()
+        let s : Sentence = sentence
+        let result : Sentence = Sentence()
         var root : Word? = checkAnalysisAndSetRoot(sentence: s, index: 0)
         var previousRoot : Word? = nil
         var nextRoot = checkAnalysisAndSetRoot(sentence: s, index: 1)
         var previousProbability, nextProbability: Double
         var isAsciifiedSame: Bool
         var candidates: [String] = []
-        for repeated in 0..<2{
-            for i in 0..<s.wordCount() {
-                candidates = []
-                isAsciifiedSame = false
-                let word = s.getWord(index: i)
-                if asciifiedSame[word.getName()] != nil{
-                    candidates.append(word.getName())
-                    candidates.append(asciifiedSame[word.getName()]!)
-                    isAsciifiedSame = true
-                }
-                if root == nil || isAsciifiedSame{
-                    if !isAsciifiedSame{
-                        candidates = candidateList(word: word)
-                    }
-                    var bestCandidate : String = word.getName()
-                    var bestRoot : Word = word
-                    var bestProbability : Double = threshold
-                    for candidate in candidates {
-                        let fsmParses = fsm.morphologicalAnalysis(surfaceForm: candidate)
-                        if rootNGram && !isAsciifiedSame{
-                            root = fsmParses.getParseWithLongestRootWord().getWord();
-                        } else {
-                            root = Word(name: candidate)
-                        }
-                        if previousRoot != nil {
-                            previousProbability = getProbability(word1: previousRoot!.getName(), word2: root!.getName())
-                        } else {
-                            previousProbability = 0.0
-                        }
-                        if nextRoot != nil {
-                            nextProbability = getProbability(word1: root!.getName(), word2: nextRoot!.getName())
-                        } else {
-                            nextProbability = 0.0
-                        }
-                        if max(previousProbability, nextProbability) > bestProbability {
-                            bestCandidate = candidate
-                            bestRoot = root!
-                            bestProbability = max(previousProbability, nextProbability)
-                        }
-                    }
-                    root = bestRoot
-                    result.addWord(word: Word(name: bestCandidate))
-                } else {
-                    result.addWord(word: word)
-                }
-                previousRoot = root
-                root = nextRoot!
-                nextRoot = checkAnalysisAndSetRoot(sentence: s, index: i + 2)
+        for i in 0..<s.wordCount() {
+            candidates = []
+            isAsciifiedSame = false
+            let word = s.getWord(index: i)
+            if asciifiedSame[word.getName()] != nil{
+                candidates.append(word.getName())
+                candidates.append(asciifiedSame[word.getName()]!)
+                isAsciifiedSame = true
             }
-            s = result
-            if repeated < 1{
-                result = Sentence()
-                previousRoot = nil
-                root = checkAnalysisAndSetRoot(sentence: s, index: 0)
-                nextRoot = checkAnalysisAndSetRoot(sentence: s, index: 1)
+            if root == nil || isAsciifiedSame{
+                if !isAsciifiedSame{
+                    candidates = candidateList(word: word)
+                }
+                var bestCandidate : String = word.getName()
+                var bestRoot : Word = word
+                var bestProbability : Double = threshold
+                for candidate in candidates {
+                    let fsmParses = fsm.morphologicalAnalysis(surfaceForm: candidate)
+                    if rootNGram && !isAsciifiedSame{
+                        root = fsmParses.getParseWithLongestRootWord().getWord();
+                    } else {
+                        root = Word(name: candidate)
+                    }
+                    if previousRoot != nil {
+                        previousProbability = getProbability(word1: previousRoot!.getName(), word2: root!.getName())
+                    } else {
+                        previousProbability = 0.0
+                    }
+                    if nextRoot != nil {
+                        nextProbability = getProbability(word1: root!.getName(), word2: nextRoot!.getName())
+                    } else {
+                        nextProbability = 0.0
+                    }
+                    if max(previousProbability, nextProbability) > bestProbability {
+                        bestCandidate = candidate
+                        bestRoot = root!
+                        bestProbability = max(previousProbability, nextProbability)
+                    }
+                }
+                root = bestRoot
+                result.addWord(word: Word(name: bestCandidate))
+            } else {
+                result.addWord(word: word)
             }
+            previousRoot = root
+            root = nextRoot!
+            nextRoot = checkAnalysisAndSetRoot(sentence: s, index: i + 2)
         }
         return result
     }
